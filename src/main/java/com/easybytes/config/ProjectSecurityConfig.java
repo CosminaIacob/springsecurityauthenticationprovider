58f1cc0e8f17779cad94ca7877946d0d9e20c49b
coiacob <coiacob@pentalog.com>
2023-08-25 14:36:02 +0300

    [Spring Security 6 Zero to Master along with JWT, OAUTH2 Udemy] Section 6
    -added CORS configurations

--- src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,7 +18,19 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(List.of("*"));
+                    // telling that the browser can remember these configurations up to 1h,
+                    // it is going to cache these details up to 1h
+                    // usually, in prod, we can set this for 24h or 30d, based on the prod deployment cycle.
+                    configuration.setMaxAge(3600L);
+                    return configuration;
+                }))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/register").permitAll())
