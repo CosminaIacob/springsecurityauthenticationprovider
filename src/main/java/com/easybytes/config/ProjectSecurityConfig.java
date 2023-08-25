d63c5b6944012d5770932a67f72a0bb36ca9537d
coiacob <coiacob@pentalog.com>
2023-08-25 18:09:23 +0300

    [Spring Security 6 Zero to Master along with JWT, OAUTH2 Udemy] Section 6
    -added CSRF configuration

--- src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.easybytes.config;
 
+import com.easybytes.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
@@ -18,7 +23,28 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+
+        // CSRF TOKEN HANDLER
+        // with this Spring Security is generating the CSRF token for you.
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        // even if we don't mention this line, be default the handler is going to construct the same name
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+
+        http
+                // SESSION MANAGEMENT
+                // we now have an UI app with a login page and don't need the default spring security login page
+                // this tells Spring Security to create the JSESSIONID
+                // by following this session management created here
+                // and after the initial login is completed
+                // the JSESSIONID will be sent to the UI and be used for the subsequent requests
+                // without these 2 lines you have to share the credentials
+                // every time you are trying to access the secured APIs form the Angular app
+                .securityContext(context -> context.requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+
+                // CORS
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                     configuration.setAllowedMethods(List.of("*"));
@@ -30,7 +56,23 @@ public class ProjectSecurityConfig {
                     configuration.setMaxAge(3600L);
                     return configuration;
                 }))
-                .csrf(csrf -> csrf.disable())
+
+                // CSRF
+                // These are public APIs and we don't need to handle CSRF attacks because there is no sensitive information
+                // we don't need to add /notices here because it is a GET request
+                // with .httpOnlyFalse() we are telling to the Spring security
+                // to create a CSRF cookie with a configuration as HttpOnlyFalse
+                // so that my JavaScript code deployed inside the angular application can read the cookie value.
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/contact", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                // execute CsrfCookieFilter after the BasicAuthenticationFilter
+                // only after the BasicAuthenticationFilter the login operation will complete
+                // and the csrf token will be generated
+                // and the crsf token will be persisted in the response with the help of CsrfCookieFilter
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+
+                // AUTHORIZATION
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/register").permitAll())
